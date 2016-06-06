ac35dc2a19285fefaabd9cd91f2747d9123c6eea
MineRobber9000 <MineRobber9000@users.noreply.github.com>
2016-06-06 18:40:09 -0400

    Configuration

---
+++ config-skel.py
@@ -0,0 +1,9 @@
+# The configuration for MR9KBot.
+SERVER = "" # The domain name of the IRC server
+PORT = 6667 # The port the server is listening on
+NICK = "" # The nick the bot should use
+PASS = None # The password the bot should use, if there is no password, leave this value as None.
+USER = "" # The ident the bot should use.
+GITTER = False # If using on gitter, set to 'True'. (minus quotes)
+REALNAME = "" # The realname the bot should use.
+CHANNELS = [""] # A list of the channels the bot should join, minus the "#", i.e; "#retro" becomes "retro", "#freecodecamp/freecodecamp" becomes "freecodecamp/freecodecamp"
